d6707a35610e1843b4928a705ae75cec7c9ec2c7
seb <sbroussaud@boostr.net>
2016-07-13 10:29:32 +0200

    daily code

---
+++ Assets/Building.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Building : MonoBehaviour
+{
+    public Text Name;
+    public Text Count;
+    public Image Image;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/GreedEngine.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GreedEngine : MonoBehaviour
+{
+    public List<Color> Colors;
+    public List<Sprite> Sprites;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public class Resource
+    {
+        public int Color;
+        public int Level;
+    }
+
+}
+
